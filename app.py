d17ec1793b3911b013b93e26d353bdd99f41b697
msobeidat <obeidatmuthaana@gmail.com>
2026-04-17 19:37:58 +0300

    Fix formatting in app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def home():
     return jsonify({"status": "ok", "message": "Hello from gp1-demo!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)  
